6ea7737f93f44f2b7289fb7f6ccbddad5f550802
ammar1709 <ammar.a.a170903@student.uns.ac.id>
2023-12-16 15:37:54 +0700

    navbar

--- engine/routes/web.php
+++ engine/routes/web.php
@@ -7,17 +7,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/home', [HomeController::class, 'home']);
-Route::get('/about', [HomeController::class, 'about']);
-Route::get('/blog-details', [HomeController::class, 'blogdetails']);
-Route::get('/blog', [HomeController::class, 'blog']);
-Route::get('/car-details', [HomeController::class, 'carsdetails']);
-Route::get('/cars', [HomeController::class, 'cars']);
-Route::get('/contact', [HomeController::class, 'contact']);
-Route::get('/faq', [HomeController::class, 'faq']);
-Route::get('/team', [HomeController::class, 'team']);
-Route::get('/terms', [HomeController::class, 'terms']);
-Route::get('/testimonials', [HomeController::class, 'testimonials']);
-Route::get('/auth', [HomeController::class, 'auth']);
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/blog-details', [HomeController::class, 'blogdetails'])->name('blogdetails');
+Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+Route::get('/car-details', [HomeController::class, 'carsdetails'])->name('carsdetails');
+Route::get('/cars', [HomeController::class, 'cars'])->name('cars');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/faq', [HomeController::class, 'faq'])->name('faq');
+Route::get('/team', [HomeController::class, 'team'])->name('team');
+Route::get('/terms', [HomeController::class, 'terms'])->name('terms');
+Route::get('/testimonials', [HomeController::class, 'testimonials'])->name('testimonials');
+Route::get('/auth', [HomeController::class, 'auth'])->name('auth');
+
 
 //Auth Post
 Route::post('/register', [AuthController::class, 'register'])->name('register');
