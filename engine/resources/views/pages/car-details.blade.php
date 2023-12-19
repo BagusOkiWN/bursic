47eea12b09c9294a683f709eaf74fd45c0f321bf
BagusOkiWN <bagusoki@student.uns.ac.id>
2023-12-19 21:15:06 +0700

    perbaikan car details

--- engine/resources/views/pages/car-details.blade.php
+++ engine/resources/views/pages/car-details.blade.php
@@ -17,15 +17,15 @@
 
 @section('content')
      <!-- ***** Call to Action Start ***** -->
-    <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/banner-image-1-1920x500.jpg)">
+    <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/mstg.jpg)">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
                     <div class="cta-content">
                         <br>
                         <br>
-                        <h2><small><del>$12 999</del></small> <em>$11 779</em></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                        <h2>CAR <em>DETAILS</em></h2>
+                        <p></p>
                     </div>
                 </div>
             </div>
@@ -49,21 +49,6 @@
                 <div class="carousel-item active">
                   <img class="d-block w-100" src="assets/images/car-image-1-1200x600.jpg" alt="First slide">
                 </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="assets/images/car-image-1-1200x600.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="assets/images/car-image-1-1200x600.jpg" alt="Third slide">
-                </div>
-              </div>
-              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
             </div>
             
             <br>
